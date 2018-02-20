cae7268a4ed3647cab6b7189f1a48bc2c5323911
Sebastien Loisel <sebastien.loisel@altran.com>
2018-02-20 12:32:27 +0100

    [ARM-16] Modification du Fit_Install.sh pour création automatique de la base GIT

--- Git_install.sh
+++ Git_install.sh
@@ -1,16 +1,19 @@
 mkdir -p ~/armada/Dev_code_version_SITL/ros_catkin_ws/src
-cd ~/armada/Dev_code_version_SITL/ros_catkin_ws/src
-#git init 
+sudo chmod 777 ~/armada/Dev_code_version_SITL
+git init 
 #git submodule add   https://github.com/erlerobot/ardupilot_sitl_gazebo_plugin
-git submodule add https://github.com/tu-darmstadt-ros-pkg/hector_gazebo/
-git submodule add -b sonar_plugin https://github.com/erlerobot/rotors_simulator
-git submodule add https://github.com/PX4/mav_comm.git
-git submodule add https://github.com/ethz-asl/glog_catkin.git
-git submodule add https://github.com/catkin/catkin_simple.git
-git submodule add https://github.com/erlerobot/mavros.git
-git submodule add -b indigo-devel https://github.com/ros-simulation/gazebo_ros_pkgs.git
-git submodule add https://github.com/erlerobot/gazebo_cpp_examples
-git submodule add https://github.com/erlerobot/gazebo_python_examples
-git submodule add https://github.com/wilselby/ROS_quadrotor_simulator
+cd ~/armada/Dev_code_version_SITL/ros_catkin_ws/src
+git submodule add https://github.com/tu-darmstadt-ros-pkg/hector_gazebo
+git submodule add -b sonar_plugin https://github.com/erlerobot/rotors_simulator 
+git submodule add https://github.com/PX4/mav_comm.git 
+git submodule add https://github.com/ethz-asl/glog_catkin.git 
+git submodule add https://github.com/catkin/catkin_simple.git 
+git submodule add https://github.com/erlerobot/mavros.git 
+git submodule add -b indigo-devel https://github.com/ros-simulation/gazebo_ros_pkgs.git 
+git submodule add https://github.com/erlerobot/gazebo_cpp_examples 
+git submodule add https://github.com/erlerobot/gazebo_python_examples  
+git submodule add https://github.com/wilselby/ROS_quadrotor_simulator 
 cd ~/armada/Dev_code_version_SITL
-git submodule add -b gazebo https://github.com/erlerobot/ardupilot
+git submodule add -b gazebo https://github.com/erlerobot/ardupilot 
+cd ..
+git submodule update --init --recursive
