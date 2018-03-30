5e19878740616872ff6666cc6e36dc884147d3f0
Livia <liviavparente@hotmail.com>
2018-03-30 13:42:58 +0200

    Script de lancement de docker avec télécommande

--- Docker_script.sh
+++ Docker_script.sh
@@ -1,5 +1,5 @@
-docker run -it --privileged \
+docker run -it --privileged --device=/dev/ttyUSB0\
  	     -v ~/armada:/home/user:rw \
 	     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 	     -e DISPLAY=unix$DISPLAY \
-	     --name=container_simulation cutkiller09/armada:part1 /bin/bash
+	     --name=container_simulation cutkiller09/armada:part3 /bin/bash
